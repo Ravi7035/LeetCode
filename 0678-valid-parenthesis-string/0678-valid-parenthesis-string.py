174ef8e6aa7c81a4cf46d578a5c928de5b1f85c9
Ravi7035 <ravimuliki7035@gmail.com>
2026-02-21 23:09:56 +0530

    Time: 0 ms (100%), Space: 12.5 MB (55.48%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -28,9 +28,36 @@ class Solution(object):
             else:
                 return False
 
-                
-
         return not Openstack
+
+        #using greedy appraoch
+
+        low=0
+        high=0
+        
+        for char in s:
+
+            if char=="(":
+                low+=1
+                high+=1
+
+            elif char==")":
+                low-=1
+                high-=1
+
+            else:
+                low-=1
+                high+=1
+
+            if high < 0:
+
+                return False
+            elif low <0:
+                low=0
+
+        return low==0
+
+
 
 
         
