8edb69d0f7eaba4b0886d29a95e256e5468af602
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-05 08:56:10 +0530

    Time: 15 ms (72.04%), Space: 12.8 MB (13.98%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def minOperations(self, s):
+        """
        #doing for 0's 
         alt=0
         min_operations_zeroes=0
@@ -20,6 +21,17 @@ class Solution(object):
             alt=1 if alt==0 else 0
 
         return min(min_operations_ones,min_operations_zeroes)
+        """
+
+        #optimized and single pass solution 
+        mismatches=0
+        for i in range(len(s)):
+            expected=str(i%2)
+
+            if s[i]!=expected:
+                mismatches+=1
+
+        return min(mismatches,len(s)-mismatches) #since if mismatches the first pattern its automatically falls on second pattern
         
 
 
