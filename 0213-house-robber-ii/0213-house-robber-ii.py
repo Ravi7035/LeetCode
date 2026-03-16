b6b1fc9a5e35a1095a4df9eb61b43ac7ad745800
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-16 21:00:21 +0530

    Time: 4 ms (8.2%), Space: 12.3 MB (81.86%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,21 +4,20 @@ class Solution(object):
         if n==1:
             return nums[0]
         def solve(arr):
+           
             m=len(arr)
+            if m==1:
+                return arr[0]
+        
             dp=[-1]*m
-            def robb(i):
-                if i==0:
-                    return arr[0]
-                if i<0:
-                    return 0
-                if dp[i]!=-1:
-                    return dp[i]
-                pick=arr[i]+robb(i-2)
-                leave=robb(i-1)
-                dp[i]=max(pick,leave)
-                return dp[i]
-            return robb(m-1)
-
+            def robb():
+                dp[0]=arr[0]
+                dp[1]=max(arr[0],arr[1])
+                for i in range(2,m):
+                    dp[i]=max(dp[i-2]+arr[i],dp[i-1])
+                return dp[m-1]
+            return robb()
+            
         case1=solve(nums[:-1])
         case2=solve(nums[1:])
         return max(case1,case2)
