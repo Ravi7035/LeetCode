1e94ed581196165ccab7a0f786ee7fcc1b14e431
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-16 16:26:21 +0530

    Time: 0 ms (100%), Space: 12.3 MB (96.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def climbStairs(self, n):
+        """
         dp=[-1]*n
         def solve(i):
             if i ==n:
@@ -10,6 +11,23 @@ class Solution(object):
                 return dp[i]
             dp[i]=solve(i+1)+solve(i+2)
             return dp[i]
-
         return solve(0)
-       
+        """
+        #optimized o(1) space solution(tabulation method)
+        prev1=1
+        prev2=2
+        if n<=2:
+            return n
+        for i in range(3,n+1):
+            curr=prev1+prev2
+            prev1=prev2
+            prev2=curr
+
+        return prev2
+            
+
+        
+            
+            
+            
+
