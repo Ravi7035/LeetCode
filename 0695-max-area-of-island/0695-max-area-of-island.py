d1e6156ec4580ccae2c5fc8a2341cdff7c9c4f50
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-17 10:41:53 +0530

    Time: 24 ms (82.76%), Space: 15.2 MB (32.37%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        rows=len(grid)
+        columns=len(grid[0])
+        
+        def dfs(r,c):
+            if r<0 or c <0 or r >=rows or c>=columns or grid[r][c]==0:
+                return 0
+
+            grid[r][c]=0
+
+            area=1
+            area+=dfs(r-1,c)
+            area+=dfs(r,c+1)
+            area+=dfs(r,c-1)
+            area+=dfs(r+1,c)
+
+            return area
+        max_area=0
+        for r in range(rows):
+            for c in range(columns):
+                if grid[r][c]==1:
+                    max_area=max(max_area,dfs(r,c))
+        return max_area
+
+        return max_area
+
+
+            
+        
+     
