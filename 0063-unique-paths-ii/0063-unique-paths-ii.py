fb3360bb6e86a9af0e9f1701aa3d7e1741096eaa
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-17 15:43:22 +0530

    Time: 1 ms (47.1%), Space: 12.5 MB (63.82%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,6 +2,7 @@ class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
         m=len(obstacleGrid)
         n=len(obstacleGrid[0])
+        """
         memo={}
         def dfs(r,c):
             if r < 0 or c < 0 or obstacleGrid[r][c] == 1:
@@ -10,10 +11,30 @@ class Solution(object):
                 return 1
             if (r, c) in memo:
                 return memo[(r, c)]
-
             memo[(r,c)]=dfs(r-1,c)+dfs(r,c-1)
             return memo[(r,c)]
-
         return dfs(m-1,n-1)
+        """
+
+        #tabualtion method
+        dp=[[0]*n for _ in range(m)]
+        if obstacleGrid[0][0]==1:
+            return 0
+        dp[0][0]=1
+
+        for r in range(m):
+            for c in range(n):
+                if obstacleGrid[r][c]==1:
+                    dp[r][c]=0
+                else:
+                    if r >0:
+                        dp[r][c]+=dp[r-1][c]
+                    if c >0:
+                        dp[r][c]+=dp[r][c-1]
+
+        return dp[m-1][n-1]
+            
+
+
        
         
