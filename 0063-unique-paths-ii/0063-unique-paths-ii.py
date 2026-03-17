6b85a89e6f98f9798ca15440e8ac69949bc73396
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-17 15:25:57 +0530

    Time: 2 ms (38.45%), Space: 12.8 MB (5.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,16 +4,16 @@ class Solution(object):
         n=len(obstacleGrid[0])
         memo={}
         def dfs(r,c):
-            if r >= m or c >= n or obstacleGrid[r][c] == 1:
+            if r < 0 or c < 0 or obstacleGrid[r][c] == 1:
                 return 0
-            if r==m-1 and c==n-1:
+            if r==0 and c==0:
                 return 1
             if (r, c) in memo:
                 return memo[(r, c)]
 
-            memo[(r,c)]=dfs(r+1,c)+dfs(r,c+1)
+            memo[(r,c)]=dfs(r-1,c)+dfs(r,c-1)
             return memo[(r,c)]
 
-        return dfs(0,0)
+        return dfs(m-1,n-1)
        
         
