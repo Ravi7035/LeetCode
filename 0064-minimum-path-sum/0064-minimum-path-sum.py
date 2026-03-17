143d14ae30c867b148e6754da3a3e7d23376bb2a
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-17 17:45:21 +0530

    Time: 19 ms (37.72%), Space: 14.1 MB (57.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,6 +3,7 @@ class Solution(object):
         #using memoization 
         m=len(grid)
         n=len(grid[0])
+        """
         minimum_sum=0
         memo={}
         def dfs(r,c):
@@ -20,6 +21,23 @@ class Solution(object):
             return memo[(r,c)]
 
         return dfs(m-1,n-1)
+        """
+
+        #using tabulation method
+        dp=[[0]*n for _ in range(m)]
+        dp[0][0]=grid[0][0]
+        for r in range(m):
+            for c in range(n):
+                if r ==0 and c==0:
+                    continue
+                elif r==0:
+                    dp[r][c]=grid[r][c]+dp[r][c-1]
+                elif c==0:
+                    dp[r][c]=grid[r][c]+dp[r-1][c]
+                else:
+                    dp[r][c]=grid[r][c]+min(dp[r-1][c],dp[r][c-1])
+        return dp[m-1][n-1]
+                
             
             
 
