8e54661c81eeb381b96f08891377152d4127dcfc
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-18 11:48:39 +0530

    Time: 4 ms (63.88%), Space: 12.9 MB (80.55%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def minimumTotal(self, triangle):
+        """
         n=len(triangle)
         memo={}
         
@@ -15,6 +16,20 @@ class Solution(object):
             return memo[(i,j)]
 
         return solve(0,0)
+        """
+
+        
+        #tabular approach 
+        dp=triangle[-1]
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                dp[j]=triangle[i][j]+min(dp[j],dp[j+1])
+
+        return dp[0]
+        
+
+
+
 
             
         
