7473676339cd35d1210d6d7f82c9e32871f946bb
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-22 13:03:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (77.67%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def findRotation(self, mat, target):
+        """
         n=len(mat)
         def isTrue(matrix):
             for row in matrix:
@@ -18,6 +19,37 @@ class Solution(object):
                 return True
 
         return False
+        """
+
+        #single pass method
+
+        #comparing the exact element when the mat is rotated by 4 angles 0,90,180,270
+
+        n=len(mat)
+
+        rot0=rot90=rot180=rot270=True
+
+        for i in range(n):
+            for j in range(n):
+                
+                if mat[i][j] != target[i][j]:
+                    rot0 = False
+                
+                if mat[i][j] != target[j][n-1-i]:
+                    rot90 = False
+                
+                if mat[i][j] != target[n-1-i][n-1-j]:
+                    rot180 = False
+                
+                if mat[i][j] != target[n-1-j][i]:
+                    rot270 = False
+
+        return rot0 or rot90 or rot180 or rot270
+        
+
+
+
+
 
         
 
