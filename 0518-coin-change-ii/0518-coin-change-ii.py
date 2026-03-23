01c3bb419b9bce092deaba0f9ba533f5606909d1
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-23 12:55:15 +0530

    Time: 235 ms (69.24%), Space: 12.4 MB (99.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,28 +2,12 @@ class Solution(object):
     def change(self, amount, coins):
         #memoization method 
         n=len(coins)
-        dp=[[0]*(amount+1) for _ in range(n)]
+        dp=[0]*(amount+1)
 
-        #initializing the dp state 
-        for i in range(n):
-            dp[i][0]=1
+        dp[0]=1
 
-        for t in range(amount+1):
-            if t % coins[0] == 0:
-                dp[0][t] = 1
-            
-        for index in range(1,n):
-            for target in range(amount+1):
+        for coin in coins:
+            for target in range(coin,amount+1):
+                dp[target]+=dp[target-coin]
 
-                not_take=dp[index-1][target]
-
-                take=0
-
-                if coins[index] <= target:
-
-                    take=dp[index][target-coins[index]]
-
-                dp[index][target]=not_take+take
-
-
-        return dp[n-1][amount]
+        return dp[amount]
