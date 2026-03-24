986abadb02ca69593091c54c35adbdb4aea60bc7
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-24 16:24:40 +0530

    Time: 448 ms (83.59%), Space: 14 MB (79.69%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def minInsertions(self, s):
+        m=len(s)
+        dp=[[-1]*m for _ in range(m)]
+
+        #initializing the dp state
+
+        for i in range(m):
+            dp[i][i]=1
+        
+
+        for length in range(2,m+1):
+            for i in range(m-length+1):
+                j=i+length-1
+
+                if s[i]==s[j]:
+                    
+                    dp[i][j]=2+dp[i+1][j-1] if length > 2 else 2 
+
+                else:
+
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+
+
+        return m-dp[0][m-1]
+        
+
+           
+
+
+
+        
