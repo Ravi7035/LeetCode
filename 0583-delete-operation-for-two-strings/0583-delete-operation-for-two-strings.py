ac1cd6f04f8aebade6890fee511c2a2d22e5d638
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-24 18:41:25 +0530

    Time: 153 ms (52.75%), Space: 14.5 MB (61.93%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -3,28 +3,21 @@ class Solution(object):
 
         m=len(word1)
         n=len(word2)
-        dp=[[-1]*(n+1) for i in range(m+1)]
+        dp=[[0]*(n+1) for i in range(m+1)]
 
-        def solve(index1,index2):
-
-            if index1 ==m or index2==n:
-                return 0
-
-            if dp[index1][index2]!=-1:
-                return dp[index1][index2]
+        for index1 in range(1,m+1):
+            for index2 in range(1,n+1):
 
 
-            if word1[index1]==word2[index2]:
+                if word1[index1-1]==word2[index2-1]:
 
-                dp[index1][index2]=1+solve(index1+1,index2+1)
-            
-            else:
-    
-                dp[index1][index2]=max(solve(index1+1,index2),solve(index1,index2+1))
+                    dp[index1][index2]=1+dp[index1-1][index2-1]
+                
+                else:
+        
+                    dp[index1][index2]=max(dp[index1-1][index2],dp[index1][index2-1])
 
-            return dp[index1][index2]
-
-        res=solve(0,0)
+        res=dp[m][n]
 
         return m-res+(n-res)
         
