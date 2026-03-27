a44ea8d0e8aac58e0a8b686130a8b474178aceeb
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-27 11:07:30 +0530

    Time: 0 ms (100%), Space: 12.3 MB (87.5%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -1,50 +1,77 @@
 class Solution(object):
     def areSimilar(self, mat, k):
-        temp_mat=[row[:] for row in mat]
+        # temp_mat=[row[:] for row in mat]
 
-        k=k%len(mat[0])
+        # k=k%len(mat[0])
 
-        def left_rotate(row_value):
+        # def left_rotate(row_value):
             
-            temp=mat[row_value][0]
+        #     temp=mat[row_value][0]
 
-            for i in range(len(mat[0])-1):
+        #     for i in range(len(mat[0])-1):
 
-                mat[row_value][i]=mat[row_value][i+1]
+        #         mat[row_value][i]=mat[row_value][i+1]
             
-            mat[row_value][-1]=temp
+        #     mat[row_value][-1]=temp
 
 
-        def right_rotate(row_value):
+        # def right_rotate(row_value):
 
-            temp=mat[row_value][-1]
+        #     temp=mat[row_value][-1]
 
-            for i in range(len(mat[0])-1,0,-1):
+        #     for i in range(len(mat[0])-1,0,-1):
 
-                mat[row_value][i]=mat[row_value][i-1]
+        #         mat[row_value][i]=mat[row_value][i-1]
 
-            mat[row_value][0]=temp
+        #     mat[row_value][0]=temp
 
 
-        while k:
+        # while k:
 
-            for i in range(len(mat)):
+        #     for i in range(len(mat)):
                 
-                if i%2==0:
-                    left_rotate(i)
+        #         if i%2==0:
+        #             left_rotate(i)
 
-                else:
-                    right_rotate(i)
+        #         else:
+        #             right_rotate(i)
 
-            k-=1
+        #     k-=1
 
 
-        if temp_mat==mat:
+        # if temp_mat==mat:
             
-            return True
+        #     return True
 
-        else:
-            return False
+        # else:
+        #     return False
 
        
+
+       #optimization this through checking are they in correct positions
+
+        m=len(mat)
+        n=len(mat[0])
+
+        k=k%n
+
+        for i in range(m):
+            for j in range(n):
+
+                if i%2==0:
+
+                    if mat[i][j] != mat[i][(j+k)%n]:
+                        return False
+
+                else:
+
+                    if mat[i][j] != mat[i][(j-k)%n]:
+                        return False
+
+        return True
+
+        
+
+
+
         
