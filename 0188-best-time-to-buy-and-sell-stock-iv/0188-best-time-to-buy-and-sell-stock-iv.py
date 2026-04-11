d5395518e21175d0f5f2fcd3eb6af6300356834e
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-04-11 23:57:01 +0530

    Time: 111 ms (17.01%), Space: 18.8 MB (15.37%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def maxProfit(self, k, prices):
+       
+        n=len(prices)
+
+        dp=[[[0]*(k+1) for _ in range(2)] for _ in range(n+1)]
+        
+        #base cases 
+
+        for index in range(n-1,-1,-1):
+            for canBuy in range(2):
+                for transactions in range(1,k+1):
+
+                    if canBuy:
+                        profit=max(-prices[index]+dp[index+1][0][transactions],0+dp[index+1][1][transactions])
+
+                    else:
+                        profit=max(prices[index]+dp[index+1][1][transactions-1],0+dp[index+1][0][transactions])
+
+                    dp[index][canBuy][transactions]=profit
+
+
+        return dp[0][1][k]
+
+            
+
+
+       
+        
+        
