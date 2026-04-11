c5d09085a2baf96cee77e75913b60067fc685cd1
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-04-11 22:58:18 +0530

    Time: 19 ms (5.06%), Space: 13.9 MB (42.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,29 +1,18 @@
 class Solution(object):
     def maxProfit(self, prices):
         n=len(prices)
-        dp=[[0]*2 for _ in range(n+1)]
 
-        #base cases
-
-        dp[n][0]=dp[n][1]=0
+        next=[0,0]
 
 
         for index in range(n-1,-1,-1):
-            for canBuy in range(2):
+            curr=[0,0]       
+            curr[1]=max(-prices[index]+next[0],0+next[1])
+            curr[0]=max(prices[index]+next[1],0+next[0])
 
-                if canBuy:
-                    
-                    
-                        profit=max(-prices[index]+dp[index+1][0],0+dp[index+1][1])
+            next=curr
 
-                else:
-                  
-                        profit=max(prices[index]+dp[index+1][1],0+dp[index+1][0])
-
-
-                dp[index][canBuy]=profit
-
-        return dp[0][1]
+        return next[1]
 
 
         
