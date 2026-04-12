e17c410d3816b21c4ac27349f45a3cb06841fd13
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-04-12 23:54:22 +0530

    Time: 5 ms (33.41%), Space: 15.7 MB (20.62%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        n=len(prices)
+        dp=[[-1]*2 for _ in range(n+1)]
+        def solve(index,canBuy):
+            if index>=n:
+                return 0
+
+            if dp[index][canBuy] !=-1:
+                return dp[index][canBuy]
+            
+            if canBuy:
+
+                profit=max(-prices[index]+solve(index+1,0),0+solve(index+1,1))
+
+            else:
+                profit=max(prices[index]+solve(index+2,1),0+solve(index+1,0))
+
+            dp[index][canBuy]=profit
+
+            return dp[index][canBuy]
+
+        return solve(0,1)
+       
+        
