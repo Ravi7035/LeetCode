fceedb70b9a2c04b61872b1fcd0fe6ba515c1ec9
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-04-13 02:18:50 +0530

    Time: 1275 ms (12.5%), Space: 37.4 MB (12.5%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -5,7 +5,7 @@ class Solution(object):
 
         dp=[[[-1]*27 for _ in range(27)] for _ in range(n+1)]
 
-
+       
         def get_index(x):
 
             if x==-1:
@@ -14,25 +14,22 @@ class Solution(object):
             return ord(x)-ord('A')
 
 
-    
-        #function to find the position of the particular letter
         def pos(x):
-            return (ord(x) - ord('A')) // 6, (ord(x) - ord('A')) % 6
+            if x==-1:
+                return (0,0)
+            v=ord(x)-ord('A')
+            return (v // 6 ,v % 6)
 
         def dist(a,b):
-
-            if a==-1 or b==-1:
+            if a==-1:
                 return 0
-
             x1,y1=pos(a)
             x2,y2=pos(b)
 
-            return abs(x1-x2)+abs(y1-y2)
+            return abs(x1-x2) + abs(y1-y2)
 
         def solve(index,f1_pos,f2_pos):
 
-            if f2_pos > f1_pos:
-                f1_pos,f2_pos=f2_pos,f1_pos
 
             if index==n:
 
@@ -45,20 +42,21 @@ class Solution(object):
 
                 return dp[index][i][j]
 
+            #finger_one
+
             cost1=dist(f1_pos,word[index])+solve(index+1,word[index],f2_pos)
+
+            #finger_two
 
             cost2=dist(f2_pos,word[index])+solve(index+1,f1_pos,word[index])
 
-           
 
             dp[index][i][j]=min(cost1,cost2)
 
             return dp[index][i][j]
 
+
         return solve(0,-1,-1)
-        
 
-
-
-
+     
         
