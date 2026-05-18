3dc03c52b3514f299fb910638cf5c2c08b377d1f
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-05-18 18:30:52 +0530

    Time: 267 ms (56.92%), Space: 12.5 MB (36.92%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -6,35 +6,31 @@ class Solution(object):
         :rtype: int
         """
         n=len(arr)
-        dp=[-1]*(n)
+        dp=[0]*(n+1)
         INT_MIN=-2**31
-        def solve(i):
 
-            if i==n:
-                return 0
+        dp[n]=0
 
-            if dp[i] !=-1:
-                return dp[i]
+       
+        maximum=INT_MIN
 
+        for i in range(n-1,-1,-1):
             current_max=0
-            maximum=INT_MIN
             for j in range(i,min(i+k,n)):
 
                 current_max=max(current_max,arr[j])
 
                 length=j-i+1
 
-                total_cost=current_max*length+solve(j+1)
+                total_cost=current_max*length+dp[j+1]
 
                 maximum=max(maximum,total_cost)
 
 
             dp[i]=maximum
 
-            return dp[i]
 
-
-        return solve(0)
+        return dp[0]
 
             
 
