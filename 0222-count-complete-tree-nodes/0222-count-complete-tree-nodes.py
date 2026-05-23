e0694c6aff3f65303aa38979f884f0dbb32b26c6
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-05-23 16:14:09 +0530

    Time: 0 ms (100%), Space: 28.5 MB (12.92%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -1,28 +1,42 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
+class Solution:
+
     def countNodes(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        node_sum=[0]
-        def solve(root):
 
-            if not root:
-                return
+        if not root:
+            return 0
 
-            node_sum[0]+=1
+        left_height = self.findLeftHeight(root)
+        right_height = self.findRightHeight(root)
 
-            solve(root.left)
-            solve(root.right)
+        # Perfect binary tree
+        if left_height == right_height:
 
-        solve(root)
-        return node_sum[0]
+            return (2 ** left_height) - 1
 
+        return (
+            1
+            + self.countNodes(root.left)
+            + self.countNodes(root.right)
+        )
 
-        
+    def findLeftHeight(self, node):
+
+        height = 0
+
+        while node:
+
+            height += 1
+            node = node.left
+
+        return height
+
+    def findRightHeight(self, node):
+
+        height = 0
+
+        while node:
+
+            height += 1
+            node = node.right
+
+        return height
