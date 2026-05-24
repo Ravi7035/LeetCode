a4d624071dd53a07a918bbab0c90121b73c85216
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-05-24 16:51:34 +0530

    Time: 4 ms (91.39%), Space: 17.3 MB (74.53%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        inorder_map={}
+        #building inorder map at first
+
+        for i in range(len(inorder)):
+            inorder_map[inorder[i]]=i
+
+        post_index=[len(postorder)-1]
+
+        def helper(left,right):
+
+            if left > right:
+                return None
+
+            root_val=postorder[post_index[0]]
+
+            post_index[0]-=1
+
+            root=TreeNode(root_val)
+
+            mid=inorder_map[root_val]
+            root.right=helper(mid+1,right)
+            root.left=helper(left,mid-1)
+
+            
+            return root
+
+        return helper(0,len(inorder)-1)
+
+        
