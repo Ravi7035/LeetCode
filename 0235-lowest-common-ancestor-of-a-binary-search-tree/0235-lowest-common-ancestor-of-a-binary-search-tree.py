d01cb2d353c961fea1a1076184038bb0552eeafd
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-05-28 12:26:24 +0530

    Time: 70 ms (5.61%), Space: 20.4 MB (45.4%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        def path(node,descendant,temp):
+
+            if not node:
+                return
+
+            temp.append(node)
+
+            if node.val==descendant.val:
+                return temp[:]
+
+            left=path(node.left,descendant,temp)
+
+            if left:
+                return left
+            right=path(node.right,descendant,temp)
+
+            if right:
+                return right
+
+            temp.pop()
+
+        first_descendant=path(root,p,[])
+        second_descendant=path(root,q,[])
+
+        second_descendant=set(second_descendant)
+
+        for i in range(len(first_descendant)-1,-1,-1):
+
+            if first_descendant[i]  in second_descendant:
+
+                return first_descendant[i]
+
+
+        
+
+        
+        
