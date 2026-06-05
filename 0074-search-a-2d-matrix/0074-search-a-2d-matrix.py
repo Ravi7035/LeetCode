b45254c452332ede0e725c1c03b97f10ca01c631
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-06-05 21:51:24 +0530

    Time: 0 ms (100%), Space: 12.6 MB (30.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,17 +8,23 @@ class Solution(object):
         m=len(matrix)
         n=len(matrix[0])
 
-        row=0
-        col=n-1
+        left=0
+        right=m*n -1
 
-        while row < m and col >=0:
+        while left <= right:
+            
+            mid=(left+right)//2
 
+            row=mid//n
+            col=mid%n
+            
             if matrix[row][col]==target:
                 return True
+
             elif matrix[row][col] < target:
-                row+=1
+                left=mid+1
+
             else:
-                col-=1
+                right=mid-1
 
         return False
-                
