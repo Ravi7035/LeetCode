f8f94f25908fa8d8374aada19fc13e66a4d785c3
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-06-06 10:21:11 +0530

    Time: 111 ms (11.04%), Space: 12.8 MB (2.39%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -4,36 +4,49 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        #prefix sum
-        Total_sum=0
-        prefix_sum=[]
-        for i in range(len(nums)):
-            if i==0:
-                prefix_sum.append(0)
-            else:
-                Total_sum+=nums[i-1]
-                prefix_sum.append(Total_sum)
+        #brute one
 
-        #suffix sum
-        Total_sum=0
-        suffix_sum=[]
+        # #prefix sum
+        # Total_sum=0
+        # prefix_sum=[]
+        # for i in range(len(nums)):
+        #     if i==0:
+        #         prefix_sum.append(0)
+        #     else:
+        #         Total_sum+=nums[i-1]
+        #         prefix_sum.append(Total_sum)
 
-        for i in range(len(nums)-1,-1,-1):
-            if i==len(nums)-1:
-                suffix_sum.append(0)
-            else:
-                Total_sum+=nums[i+1]
-                suffix_sum.append(Total_sum)
+        # #suffix sum
+        # Total_sum=0
+        # suffix_sum=[]
 
-        suffix_sum.reverse()
+        # for i in range(len(nums)-1,-1,-1):
+        #     if i==len(nums)-1:
+        #         suffix_sum.append(0)
+        #     else:
+        #         Total_sum+=nums[i+1]
+        #         suffix_sum.append(Total_sum)
 
-        #output block
+        # suffix_sum.reverse()
+
+        # #output block
+        # ans=[]
+        # for i in range(len(nums)):
+        #     ans.append(abs(prefix_sum[i]-suffix_sum[i]))
+
+
+        # return ans
+
+        #optimized one without any extra space
+
         ans=[]
-        for i in range(len(nums)):
-            ans.append(abs(prefix_sum[i]-suffix_sum[i]))
 
+        for i in range(len(nums)):
+            ans.append(abs(sum(nums[:i])-sum(nums[i+1:])))
 
         return ans
+
+
                 
             
         
