fab8d25377d0a8723d75db8c30448f06831881b0
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-06-06 22:05:33 +0530

    Time: 0 ms (100%), Space: 12.3 MB (60.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,15 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        memo={}
-        def dfs(r, c):
-            if r >= m or c >= n:
-                return 0
-            
-            if r == m-1 and c == n-1:
-                return 1
+        dp=[[0]*n for _ in range(m)]
 
-            if (r,c) in memo:
-                return memo[(r,c)]
-            
-            memo[(r,c)]=dfs(r, c+1) + dfs(r+1, c)
-            return memo[(r,c)]
+        for i in range(m):
+            dp[i][0]=1
 
-        return dfs(0, 0)
+        for j in range(n):
+            dp[0][j]=1
+        
+        for i in range(1,m):
+            for j in range(1,n): 
+                dp[i][j]=dp[i-1][j] +dp[i][j-1]
+
+        return dp[m-1][n-1]
