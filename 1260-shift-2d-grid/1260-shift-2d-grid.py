5eb8f20ceb6e63f62db9177a8d86ab7837c7b953
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-07-20 15:21:07 +0530

    Time: 17 ms (27.37%), Space: 12.6 MB (42.11%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        m, n = len(grid), len(grid[0])
+        total = m * n
+        k %= total
+
+        ans = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                idx = i * n + j
+                new_idx = (idx + k) % total
+                r = new_idx // n
+                c = new_idx % n
+                ans[r][c] = grid[i][j]
+
+        return ans
