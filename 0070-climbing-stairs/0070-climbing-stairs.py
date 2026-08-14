8e17a59747367d0b8af31118f35fdcf4dafab741
Ravi.Muliki <ravimuliki7035@gmail.com>
2026-08-14 11:08:51 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,33 +1,15 @@
 class Solution(object):
     def climbStairs(self, n):
-        """
-        dp=[-1]*n
-        def solve(i):
-            if i ==n:
-                return 1
-            if i > n:
-                return 0
-            if dp[i] !=-1:
-                return dp[i]
-            dp[i]=solve(i+1)+solve(i+2)
-            return dp[i]
-        return solve(0)
-        """
-        #optimized o(1) space solution(tabulation method)
-        prev1=1
-        prev2=2
-        if n<=2:
-            return n
-        for i in range(3,n+1):
-            curr=prev1+prev2
-            prev1=prev2
-            prev2=curr
 
-        return prev2
-            
+        dp = [0] * (n + 2)
+
+        dp[n] = 1
+        dp[n + 1] = 0
+
+        for step in range(n - 1, -1, -1):
+            dp[step] = dp[step + 1] + dp[step + 2]
+
+        return dp[0]
 
         
-            
-            
-            
-
+        
