495ebae00fcdb5f1b6dd7b05791160ac35f02fd9
Ravi.Muliki <ravimuliki7035@gmail.com>
2026-08-20 09:38:35 +0530

    Time: 4 ms (12.9%), Space: 12.3 MB (95.16%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -3,14 +3,12 @@ class Solution(object):
         arr1=[nums[0]]
         arr2=[nums[1]]
 
-        for i in range(len(nums)):
-            index=i+1
+        for i in range(2,len(nums)):
 
-            if index > 2:
-                if arr1[-1] > arr2[-1]:
-                    arr1.append(nums[i])
-                else:
-                    arr2.append(nums[i])
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
 
         return arr1+arr2
         
